6a40b05cb1e0913e90be18f3ad11dddf1a4ee3cf
041467 <sumayrajpanthee@outlook.com>
2025-06-12 09:55:32 +0545

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import { FaFacebook } from "react-icons/fa";
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <div>
+        <h1>Hello</h1>
+            <FaFacebook  style={{color:"blue"}} />
+
+       </div>
+    </>
+  )
+}
+
+export default App

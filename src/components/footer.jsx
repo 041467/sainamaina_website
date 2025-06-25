2df280ed81833e20c569362a3c452ee8e6bc87ef
041467 <sumayrajpanthee@outlook.com>
2025-06-25 13:32:15 +0545

    updated

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,9 +1,44 @@
 const Footer = () => {
   return (
     <>
-      <footer>
+      <footer
+        style={{
+          backgroundColor: "rgb(244, 231, 225)",
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          marginTop: "50px",
+        }}
+      >
         <div>
-          <div id="left-side-footer">jwes</div>
+          <div id="logoinfooter"></div>
+          <br />
+          <p>
+            The school operates with a dedicated team of experienced and
+            qualified teachers who use effective teaching methodologies aligned
+            with Nepal’s national curriculum. Equipped with essential facilities
+            and a growing focus on modern educational tools, Sainamaina
+            Secondary School ensures that every child receives a meaningful and
+            future-focused education.
+          </p>
+        </div>
+        <div>
+          Social Media
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut ullam
+            expedita earum, debitis nam, placeat soluta ipsam voluptate
+            distinctio omnis mollitia explicabo ut deleniti sunt ipsa, fuga
+            sapiente. Voluptatum, voluptate.
+          </p>
+        </div>
+        <div>
+          Butwal
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut ullam
+            expedita earum, debitis nam, placeat soluta ipsam voluptate
+            distinctio omnis mollitia explicabo ut deleniti sunt ipsa, fuga
+            sapiente. Voluptatum, voluptate.
+          </p>
         </div>
       </footer>
     </>
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,21 +1,51 @@
 const Contact = () => {
   return (
     <>
-      <h1>Contact Page</h1>
-      <h5>Address of the school and telephone number of the School.</h5>
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3358.1322880927814!2d83.36766087525308!3d27.68827477619343!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39968924ee2f33a3%3A0x40bc5cb604665862!2sSainamaina%20Parroha%20Parameshwar%20Model%20Secondary%20School!5e1!3m2!1sen!2snp!4v1750736280644!5m2!1sen!2snp"
-        width="600"
-        height="450"
-        style={{ border: 0 }}
-        allowfullscreen=""
-        loading="lazy"
-        referrerpolicy="no-referrer-when-downgrade"
-      ></iframe>
-      <div>
-        <h1>Contact Us </h1>
-        <p></p>
-        <form></form>
+      <div style={{ display: "flex", marginTop: "50px" }}>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3358.1322880927814!2d83.36766087525308!3d27.68827477619343!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39968924ee2f33a3%3A0x40bc5cb604665862!2sSainamaina%20Parroha%20Parameshwar%20Model%20Secondary%20School!5e1!3m2!1sen!2snp!4v1750736280644!5m2!1sen!2snp"
+          width="600"
+          height="450"
+          style={{ border: 0 }}
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+        ></iframe>
+        <div style={{ paddingLeft: "50px" }}>
+          <h1>Contact Us</h1>
+          <p></p>
+          <form>
+            <div>
+              <div>
+                <input type="text" placeholder="*Name" id="Name" name="Name" />
+              </div>
+              <div>
+                <input
+                  type="text"
+                  placeholder="*Address"
+                  id="Address"
+                  name="Address"
+                />
+              </div>
+            </div>
+            <br></br>
+            <input type="email" placeholder="*Email" id="Email" name="Email" />
+            <input
+              type="number"
+              placeholder="*Phone-Number"
+              id="P.N"
+              name="P.N"
+            />
+            <br></br>
+            <textarea placeholder="*Send Message"></textarea>
+            <button
+              type="submit"
+              style={{ backgroundColor: " rgb(37, 77, 112)", color: "white" }}
+            >
+              Send Message
+            </button>
+          </form>
+        </div>
       </div>
     </>
   );
